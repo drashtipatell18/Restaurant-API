cdf3bfc02ce6d4cb4ca5ec57f410125f46c0d57e
drashtipatell18 <165013408+drashtipatell18@users.noreply.github.com>
2024-09-11 12:57:02 +0530

    Update OrderMaster.php

--- app/Models/OrderMaster.php
+++ app/Models/OrderMaster.php
@@ -11,7 +11,7 @@ class OrderMaster extends Model
     use HasFactory,SoftDeletes;
     protected $table = 'order_masters';
 
-    protected $fillable = ['table_id','user_id','box_id','order_type','payment_type','status','tip','discount','delivery_cost','customer_name','person','reason'];
+    protected $fillable = ['table_id','user_id','box_id','order_type','finished_at','payment_type','status','tip','discount','delivery_cost','customer_name','person','reason'];
 
 
     protected static function booted()
