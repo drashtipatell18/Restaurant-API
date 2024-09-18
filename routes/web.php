5b56cf641e54ae48973c8f9b691af37c3b247b70
Dhruvish <165013408+drashtipatell18@users.noreply.github.com>
2024-09-18 17:55:54 +0530

    Notification Code

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChatAppController;
-
-
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\NotificationController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -21,3 +21,8 @@ Route::post('brodcastCardClicked',[ChatAppController::class,'cardClicked'])->nam
 // Chat Application
 Route::get('login',[ChatAppController::class,'chatLoggin'])->name('chat.loggin');
 Route::get('chatmsg',[ChatAppController::class,'chatPage']);
+
+
+Route::get('create', [UserController::class, 'create']);
+
+Route::get('notification', [NotificationController::class, 'notification']);
