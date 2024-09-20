895c43713653f2e7890f94e6518872b10f3ee599
Dhruvish <165013408+drashtipatell18@users.noreply.github.com>
2024-09-20 09:11:45 +0530

    Latest Auth Controller Changes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,10 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Role;
-
+use App\Models\Invite;
 use App\Models\User;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Mail;
@@ -35,17 +35,30 @@ class AuthController extends Controller
     
         // Find user by email
         $user = User::where('email', $request->input('email'))->first();
+      
 
-        
         $errorMessage = 'No se pudo ingresar al sistema. Verifica tu correo y contraseña e intenta nuevamente.';
         if (!$user) {
-            return response()->json([
-                'success' => false,
+            broadcast(new NotificationMessage('notification', $errorMessage))->toOthers();
+
+            Notification::create([
+                'user_id' => 1, // No specific user associated with this notification
                 'notification_type' => 'alert',
                 'notification' => $errorMessage,
+            ]);
+
+    
+            return response()->json([
+                'success' => false,
+                // 'alert' => $errorMessage,
             ], 401);
         }
-         
+         if (!$user) {
+            return response()->json([
+                'success' => false,
+                'message' => 'El usuario ingresado no existe en la base de datos del sistema'
+            ], 401);
+        }
     
     if ($user->status === 'Suspender') {
             return response()->json([
@@ -69,21 +82,14 @@ class AuthController extends Controller
                 'message' => 'Error decrypting password'
             ], 500);
         }
-        $adminId = $user->role_id == 1 ? $user->id : $user->admin_id;
-      
+    
+        // Check if the decrypted password matches the provided password
         if ($decrypted_password !== $request->input('password')) {
-            broadcast(new NotificationMessage('notification', $errorMessage))->toOthers();
-            Notification::create([
-                'user_id' => $user->id, 
-                'notification_type' => 'alert',
-                'notification' => $errorMessage,
-                'admin_id' => $adminId,
-                'role_id' => $user->role_id
-            ]);
+            // broadcast(new NotificationMessage('notification', $errorMessage))->toOthers();
             return response()->json([
                     'success' => false,
                     'message' => 'La contraseña ingresada no coincide con el correo electrónico proporcionado',
-                    'alert' => $errorMessage,
+                    // 'alert' => $errorMessage,
                 ], 401);
         }
     
@@ -92,12 +98,11 @@ class AuthController extends Controller
     
         $successMessage = "Bienvenido, {$user->name}. Has ingresado exitosamente al sistema.";
         broadcast(new NotificationMessage('notification', $successMessage))->toOthers();
+
         Notification::create([
             'user_id' => $user->id,
             'notification_type' => 'notification',
             'notification' => $successMessage,
-            'admin_id' => $adminId,
-            'role_id' => $user->role_id
         ]);
 
         return response()->json([
@@ -106,37 +111,25 @@ class AuthController extends Controller
             'email' => $user->email,
             'access_token' => $token,
             'role' => Role::find($user->role_id)->name,
-            'notification' => $successMessage,
+            // 'notification' => $successMessage,
             'admin_id' => $user->admin_id
         ]);
     }
-
-   
     
     public function invite(Request $request)
     {
         $validateUser = Validator::make($request->all(), [
             'role_id' => 'required|exists:roles,id',
             'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
+            'email' => 'required|email',
         ]);
 
         if ($validateUser->fails()) {
-                $errorMessage = "No se pudo enviar la invitación al usuario {$request->name}. Verifica la información e intenta nuevamente..";
-                broadcast(new NotificationMessage('notification', $errorMessage))->toOthers();
-                Notification::create([
-                    'user_id' => $request->id,
-                    'notification_type' => 'alert',
-                    'notification' => $errorMessage,
-                    'admin_id' => $request->admin_id
-                ]);
-        
-                return response()->json([
-                    'success' => false,
-                    'message' => 'Email validation fails',
-                    'error' => $validateUser->errors(),
-                    'alert' => $errorMessage
-                ], 401);
+            return response()->json([
+                'success' => false,
+                'message' => 'Validation fails',
+                'errors' => $validateUser->errors()
+            ], 422);
         }
 
         $user = User::create([
@@ -152,12 +145,11 @@ class AuthController extends Controller
         $user->save();
 
         Mail::to($user->email)->send(new FirstLoginMail($user, $token));
-    
+
         return response()->json([
             'success' => true,
             'message' => 'User invited successfully. Email sent with login instructions.',
             'user' => $user,
-            'admin_id' => $user->id,
         ], 201);
     }
 
