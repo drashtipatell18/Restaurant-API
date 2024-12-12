62141d979fe06273a4c23207c2909e39d5d26275
drashtipatell18 <165013408+drashtipatell18@users.noreply.github.com>
2024-12-12 11:23:49 +0530

    Update firstlogin.blade.php

--- resources/views/firstlogin.blade.php
+++ resources/views/firstlogin.blade.php
@@ -1,3 +1,36 @@
+<!--<!DOCTYPE html>-->
+<!--<html lang="en">-->
+
+<!--<head>-->
+<!--    <meta charset="UTF-8">-->
+<!--    <meta http-equiv="X-UA-Compatible" content="IE=edge">-->
+<!--    <meta name="viewport" content="width=device-width, initial-scale=1.0">-->
+<!--    <title>User Registration Confirmation</title>-->
+<!--</head>-->
+
+<!--<body>-->
+<!--    <p>Hello {{ $user->name }},</p>-->
+
+<!--    <p>Congratulations! Your registration for the Restaurant Website is confirmed.</p>-->
+    
+<!--    <p>To set your password, please click on the link below:</p>-->
+    
+<!--    <p>-->
+<!--        <a href="{{ url('api/set-password/' . $user->id ) }}">Set Your Password</a>-->
+<!--    </p>-->
+
+<!--    <p>If the above link doesn't work, copy and paste the following URL into your browser:</p>-->
+    
+<!--    <p>{{ url('api/set-password/' . $user->id ) }}</p>-->
+
+<!--    <p>This link will expire in 24 hours for security reasons.</p>-->
+
+<!--    <p>If you didn't request this registration, please ignore this email.</p>-->
+
+<!--    <p>Thank you!</p>-->
+<!--</body>-->
+
+<!--</html>-->
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,29 +38,29 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User Registration Confirmation</title>
+    <title>Confirmación de Registro de Usuario</title>
 </head>
 
 <body>
-    <p>Hello {{ $user->name }},</p>
+    <p>Hola {{ $user->name }},</p>
 
-    <p>Congratulations! Your registration for the Restaurant Website is confirmed.</p>
+    <p>¡Felicidades! Su registro para el sitio web del restaurante está confirmado.</p>
     
-    <p>To set your password, please click on the link below:</p>
+    <p>Para establecer su contraseña, haga clic en el siguiente enlace:</p>
     
     <p>
-        <a href="{{ url('api/set-password/' . $user->id ) }}">Set Your Password</a>
+        <a href="https://dev.cyproapp.com/enlaceAdmin/pass/{{$user->id}}">Establecer su Contraseña</a>
     </p>
 
-    <p>If the above link doesn't work, copy and paste the following URL into your browser:</p>
+    <p>Si el enlace anterior no funciona, copie y pegue la siguiente URL en su navegador:</p>
     
-    <p>{{ url('api/set-password/' . $user->id ) }}</p>
+    <p>https://dev.cyproapp.com/enlaceAdmin/pass/{{$user->id}}</p>
 
-    <p>This link will expire in 24 hours for security reasons.</p>
+    <p>Este enlace caducará en 24 horas por razones de seguridad.</p>
 
-    <p>If you didn't request this registration, please ignore this email.</p>
+    <p>Si no solicitó este registro, ignore este correo electrónico.</p>
 
-    <p>Thank you!</p>
+    <p>¡Gracias!</p>
 </body>
 
 </html>
