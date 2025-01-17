07034e49f89ba40c036d5a16fd8532084c6ba0ca
drashtipatell18 <165013408+drashtipatell18@users.noreply.github.com>
2025-01-17 11:06:47 +0530

    Update updateuser.blade.php

--- resources/views/updateuser.blade.php
+++ resources/views/updateuser.blade.php
@@ -5,21 +5,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Confirmaci贸n de registro de usuario</title>
+    <title>Confirmación  de registro de usuario</title>
 </head>
 
 <body>
     <p>Hola {{ $user->name }}!</p>
 <br><br>
     <p>Te hemos dado acceso a nuestro portal para que puedas gestionar tu viaje con nosotros y conocer todas las posibilidades que ofrece Cyproapp.</p>
-    <p>Si quieres iniciar sesi贸n en tu cuenta, haz clic en el siguiente enlace: <a href="https://dev.cyprosolution.com">https://dev.cyprosolution.com</a></p>
+    <p>Si quieres iniciar sesión  en tu cuenta, haz clic en el siguiente enlace: <a href="https://dev.cyprosolution.com">https://dev.cyprosolution.com</a></p>
     <p>Congratulations! Your registration for the Restaurant Website is confirmed.</p>
     <p>Credenciales:</p>
     <p>Correo: {{ $user->email }} </p>
 
-    <p>Contrase帽a : {{$plainPassword }}</p>
+    <p>Contraseña : {{$plainPassword }}</p>
     <p>
-    隆Disfruta!
+    ¡Disfruta!
     </p>
     <br><br><br>
 <p>
